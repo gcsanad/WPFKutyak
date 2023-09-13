d839a9ad0a50fb04f6b95842f56eba03c5fa8aed
gcsanad <71721197+gcsanad@users.noreply.github.com>
2023-09-13 10:49:40 +0200

    Add files via upload

---
+++ Kutyak/KutyakOsztaly.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutyak
+{
+    internal class KutyakOsztaly
+    {
+        int id;
+        int fajtaId;
+        int nevId;
+        int eletkor;
+        DateTime utolsoOrvosiVizsgalat;
+        string nev;
+        string eredetiNev;
+        string kutyaNev;
+
+
+        public KutyakOsztaly(string sor) {
+            string[] felosztandoSor = sor.Split(";");
+            this.id = int.Parse(felosztandoSor[0]);
+            this.fajtaId = int.Parse(felosztandoSor[1]);
+            this.nevId = int.Parse(felosztandoSor[2]);
+            this.eletkor = int.Parse(felosztandoSor[3]);
+            this.utolsoOrvosiVizsgalat = DateTime.Parse(felosztandoSor[4]);
+        }
+
+
+        public KutyakOsztaly(string sor, bool valaszto) {
+            string[] felosztandoSor = sor.Split(";");
+            if (valaszto == true)
+            {
+                this.id = int.Parse(felosztandoSor[0]);
+                this.nev = felosztandoSor[1];
+                this.eredetiNev = felosztandoSor[2];
+            }
+            else if (valaszto == false)
+            {
+                this.id = int.Parse(felosztandoSor[0]);
+                this.kutyaNev = felosztandoSor[1];
+            }
+        }
+
+
+
+
+
+
+
+
+
+        public int Id { get => id; }
+        public int FajtaId { get => fajtaId;}
+        public int NevId { get => nevId; }
+        public int Eletkor { get => eletkor; }
+        public DateTime UtolsoOrvosiVizsgalat { get => utolsoOrvosiVizsgalat; }
+        public string Nev { get => nev; }
+        public string EredetiNev { get => eredetiNev; }
+        public string KutyaNev { get => kutyaNev; }
+    }
+}
---
+++ Kutyak/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kutyak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<KutyakOsztaly> kutyaNevek = File.ReadAllLines("KutyaNevek.csv").Skip(1).Select(x => new KutyakOsztaly(x, false)).ToList();
+        private List<KutyakOsztaly> kutyak = File.ReadAllLines("Kutyak.csv").Skip(1).Select(x => new KutyakOsztaly(x)).ToList();
+        private List<KutyakOsztaly> kutyaFajtak = File.ReadAllLines("KutyaFajtak.csv").Skip(1).Select(x => new KutyakOsztaly(x, true)).ToList();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dpDatumValaszto.Text = Convert.ToString(DateTime.Now);
+            //3.Feladat
+            lblKutyanavekSzama.Content = kutyaNevek.Count;
+            //6.Feladat
+            lblAtlagEletkor.Content =  Math.Round(kutyak.Average(x => x.Eletkor), 2);
+            //7.Feladat
+            int id = kutyak.OrderByDescending(x => x.Eletkor).ToList()[0].NevId;
+            int eletkorId = kutyak.OrderByDescending(x => x.Eletkor).ToList()[0].FajtaId;
+            string nev = kutyaNevek.Where(x => x.Id == id).ToList()[0].KutyaNev;
+            string fajta = kutyaFajtak.Where(x => x.Id == eletkorId).ToList()[0].Nev;
+            lblLegidosebbKutya.Content = $"{nev}, {fajta}";
+        }
+
+        private void btnKiiratas_Click(object sender, RoutedEventArgs e)
+        {
+            lbKutyaRendeloben.ItemsSource = kutyak.Where(x => Convert.ToString(x.UtolsoOrvosiVizsgalat) == dpDatumValaszto.Text).GroupBy(x => x.KutyaNev).Select(x => $"{x.Key} : {x.Count()}");
+            
+            /*
+            if (lbKutyaRendeloben.Items.Count == 0)
+            {
+                MessageBox.Show("Ebben az időpontban nincs feljegyezve kutya");
+            }*/
+        }
+    }
+}
